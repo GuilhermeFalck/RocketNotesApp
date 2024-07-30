458b315e8640d7321ec3c38b793957d87f8c0b6c
guilherme <guilhermefalck22@gmail.com>
2024-07-30 12:54:56 -0300

    create components header

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,14 @@
 import { Container } from "./styles.js";
 
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+
 export function Details() {
   return (
     <Container>
-      <h1>Hello World!</h1>
-      <p>Olá</p>
+      <Header />
+
+      <Button title="Voltar" />
     </Container>
   );
 }
