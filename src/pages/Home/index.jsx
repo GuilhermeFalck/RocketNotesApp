bbd18cc937a01b820eca55693b67298be7798a66
guilherme <guilhermefalck22@gmail.com>
2024-07-31 14:36:53 -0300

    finished home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,10 @@
 import { FiPlus, FiSearch } from "react-icons/fi";
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
-import { Header } from "../../components/Header";
+import { Note } from "../../components/Note";
 import { Input } from "../../components/Input";
+import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
 import { ButtonText } from "../../components/ButtonText";
 
 export function Home() {
@@ -30,7 +32,19 @@ export function Home() {
         <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
       </Search>
 
-      <Content></Content>
+      <Content>
+        <Section title="Minhas Notas">
+          <Note
+            data={{
+              title: "React",
+              tags: [
+                { id: "1", name: "react" },
+                { id: "2", name: "rocketseat" },
+              ],
+            }}
+          />
+        </Section>
+      </Content>
 
       <NewNote>
         <FiPlus />
