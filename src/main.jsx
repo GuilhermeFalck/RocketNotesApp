baf41d6273fa25bf1d82c64d67b45cdbdd6e3a33
guilherme <guilhermefalck22@gmail.com>
2024-07-31 15:32:10 -0300

    finished singIn and create s singUp

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyle from "./styles/global";
 
 import theme from "./styles/theme";
 
-import { SingIn } from "./pages/SingIn";
+import { SingUp } from "./pages/SingUp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <SingIn />
+      <SingUp />
     </ThemeProvider>
   </React.StrictMode>
 );
