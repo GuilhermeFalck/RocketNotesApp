8a18c11de5f0480679e5008a984c554ef7452c47
guilherme <guilhermefalck22@gmail.com>
2024-07-31 15:02:26 -0300

    create page singIn

---
+++ src/pages/SingIn/index.jsx
@@ -0,0 +1,21 @@
+import { Container } from "./styles";
+import { FiMail, FiLock } from "react-icons/fi";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+export function SingIn() {
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerencias seus links úteis</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+      </Form>
+    </Container>
+  );
+}
